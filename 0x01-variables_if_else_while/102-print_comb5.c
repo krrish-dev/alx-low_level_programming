f78719c8a2b9c65edf806c9512c85c3c5a6f96f4
krrish_dev <justbegintoday@gmail.com>
2023-09-09 05:11:21 +0300

    update Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry Point -krrish
  * Description: Write a program that prints all possible
  * different combinations of two digits
  * Return: 0
