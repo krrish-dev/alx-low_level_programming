5ffe99cae858f306f49fa51fdb41e37831df0716
krrish_dev <justbegintoday@gmail.com>
2023-09-13 02:02:30 +0300

    Done

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,14 +2,12 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a single character to the standard output (stdout).
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * @c: The character to be written.
- *
- * Return: On success, the function returns 1 (the number of characters written).
- *         On error, it returns -1, and errno is set to indicate the error.
+ * Return: On success 1.
+ *         On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
