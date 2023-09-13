6bf829a55775143fc1bb372f3e924eb84903f238
krrish_dev <justbegintoday@gmail.com>
2023-09-13 03:02:16 +0300

    Done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,8 @@
  * Return: 1 if `c` is a letter (uppercase or lowercase), 0 otherwise.
 */
 
-int _isalpha(int c)
-{
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    return (0);
+int _isalpha(int c) {
+  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    return (1);
+  return (0);
 }
