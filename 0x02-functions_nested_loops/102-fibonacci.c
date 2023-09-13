3e408c98c423ff14d82861a0c0610f4917d74655
krrish_dev <justbegintoday@gmail.com>
2023-09-13 04:04:59 +0300

    Done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: Prints the first 50 Fibonacci numbers starting with 1 and 2 followed by a new line.
+ * Description: prints the first 50 Fibonacci numbers
+ *		starting with 1 and 2 folled by a new line
  *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
 */
 
 int main(void)
