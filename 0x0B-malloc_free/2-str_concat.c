0aaab48d4308a64d660a4e84e1304872ab4dfee0
krrish_dev <justbegintoday@gmail.com>
2023-10-05 03:27:58 +0300

    Done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,57 +1,51 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string
- * @str: The string to measure the length of
- * 
- * Return: The length of @str
-*/  
-int _strlen(char *str)
+ * _strlen - Calculates the length of a string
+ * @s: The string to measure the length of
+ *
+ * Return: The length of the string
+*/
+
+int _strlen(char *s)
 {
   int len = 0;
-
-  while (str[len] != '\0')
+  while (s[len] != '\0')
     len++;
-
-  return (len);
+  return (len); 
 }
 
-/**
- * str_concat - Concatenates two strings
+/** 
+ * str_concat - Concatenates two strings into a new allocated string
  * @s1: The first string to concatenate
  * @s2: The second string to concatenate
  *
- * Return: Pointer to the concatenated string, or NULL on failure
+ * Description: This function concatenates two strings into a newly 
+ * allocated string. It handles NULL strings by treating them as empty.
+ * The new string is null-terminated. Returns NULL on failure.
+ * 
+ * Return: Pointer to the newly allocated concatenated string, or NULL on failure
 */
 char *str_concat(char *s1, char *s2)
 {
-  int s1_len, s2_len, i;
-  char *concat_str;
+  // Determine lengths of s1 and s2
+  int len1 = _strlen(s1);
+  int len2 = _strlen(s2);
 
-  // Handle NULL inputs
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)  
-    s2 = "";
-
-  // Get lengths of s1 and s2
-  s1_len = _strlen(s1);
-  s2_len = _strlen(s2);
-  
   // Allocate memory for concatenated string
-  concat_str = malloc((s1_len + s2_len + 1) * sizeof(char));
-  if (concat_str == NULL)
+  char *concat = malloc(sizeof(char) * (len1 + len2 + 1));
+  if (!concat)
     return (NULL);
 
-  // Concatenate s1 and s2
-  for (i = 0; i < s1_len + s2_len; i++) {
-    if (i < s1_len)
-      concat_str[i] = s1[i];
-    else
-      concat_str[i] = s2[i - s1_len];
-  }
+  // Copy s1 and s2 into concat
+  for (int i = 0; i < len1; i++)
+    concat[i] = s1[i];
+  for (int i = 0; i < len2; i++) 
+    concat[len1 + i] = s2[i];
 
-  concat_str[i] = '\0'; // Null terminate
+  // Add null terminator
+  concat[len1 + len2] = '\0';
 
-  return (concat_str);
+  // Return concatenated string
+  return (concat);
 }
