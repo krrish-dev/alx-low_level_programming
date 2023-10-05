941747c580189c1787399938cb0f4adfeb41b1d8
krrish_dev <justbegintoday@gmail.com>
2023-10-05 03:22:45 +0300

    Done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,57 @@
 #include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
- */
-
-
-int _strlen(char *s)
+ * _strlen - Returns the length of a string
+ * @str: The string to measure the length of
+ * 
+ * Return: The length of @str
+*/  
+int _strlen(char *str)
 {
-	int size = 0;
+  int len = 0;
 
-	for (; s[size] != '\0'; size++)
-	;
-	return (size);
+  while (str[len] != '\0')
+    len++;
+
+  return (len);
 }
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
-
+ * str_concat - Concatenates two strings
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ *
+ * Return: Pointer to the concatenated string, or NULL on failure
+*/
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i;
-	char *m;
+  int s1_len, s2_len, i;
+  char *concat_str;
 
-	if (s1 == NULL)
-		s1 = "\0";
-	if (s2 == NULL)
-		s2 = "\0";
+  // Handle NULL inputs
+  if (s1 == NULL)
+    s1 = "";
+  if (s2 == NULL)  
+    s2 = "";
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	m = malloc((size1 + size2) * sizeof(char) + 1);
-	if (m == 0)
-		return (0);
+  // Get lengths of s1 and s2
+  s1_len = _strlen(s1);
+  s2_len = _strlen(s2);
+  
+  // Allocate memory for concatenated string
+  concat_str = malloc((s1_len + s2_len + 1) * sizeof(char));
+  if (concat_str == NULL)
+    return (NULL);
 
-	for (i = 0; i <= size1 + size2; i++)
-	{
-		if (i < size1)
-			m[i] = s1[i];
-		else
-			m[i] = s2[i - size1];
-	}
-	m[i] = '\0';
-	return (m);
+  // Concatenate s1 and s2
+  for (i = 0; i < s1_len + s2_len; i++) {
+    if (i < s1_len)
+      concat_str[i] = s1[i];
+    else
+      concat_str[i] = s2[i - s1_len];
+  }
+
+  concat_str[i] = '\0'; // Null terminate
+
+  return (concat_str);
 }
