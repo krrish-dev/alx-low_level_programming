300809e265a3ee7224e80fb2efc647a90726e5d1
krrish_dev <justbegintoday@gmail.com>
2023-10-09 03:26:55 +0300

    Done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-
-* malloc_checked - Allocates requested memory using malloc and exits if allocation fails
-
-* @b: Number of bytes to allocate
-
-* Return: Pointer to allocated memory of @size bytes, or NULL if allocation fails
+ * malloc_checked - Allocates requested memory using malloc
+ * and exits if allocation fails
+ * @b: Number of bytes to allocate
+ * Return: Pointer to allocated memory of @b bytes
+ * or NULL if allocation fails
 */
 
 void *malloc_checked(unsigned int b)
