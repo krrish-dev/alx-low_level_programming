0886c77db5fdc8a7f7c9402bbcf469bea6bc633f
krrish_dev <justbegintoday@gmail.com>
2023-10-12 22:32:13 +0300

    Done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,12 +2,18 @@
 
 
 /**
- * sum_them_all - sums variable arguments
- * @n: the number of arguments
- * @...: the integers to sum
+ * sum_them_all - Sums all the parameter arguments
+ * @n: The number of arguments passed to the function
+ * @...: A variable number of arguments to sum
+ * 
+ * Description: This function returns the sum of all the
+ * parameter arguments passed to it. The first parameter
+ * n determines how many extra arguments were passed to
+ * the function. A va_list is used to iterate over the
+ * arguments and sum them.
  *
- * Return: the integer sum
- */
+ * Return: The sum of all the arguments, or 0 if n is 0.
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 	int s = 0, i = n;
