5256f7ea6076e9ff9010e755577e6140f1c27a46
krrish_dev <justbegintoday@gmail.com>
2023-10-20 00:32:52 +0300

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 
 /**
- * list_len - Determines the length of a linked list
+ * list_len - Determines the length
+ * of a linked list
  * @head: A pointer to the first node
  *
  * Return: The size of the list
  */
+
 size_t list_len(const list_t *head)
 {
 	size_t node_count = 0;
