b63b9373bdd6b13f149fac711aff8dd82f90aeae
krrish_dev <justbegintoday@gmail.com>
2023-12-18 22:47:44 +0200

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - Calculates the length of a string
+ * @s: the string variable length to check
  *
  * Return: integer length of string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int itr = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		i++;
-	return (i);
+		itr++;
+	return (itr);
 }
 
 /**
  * append_text_to_file - appends text to a file
  * @filename: name of file to create
- * @text_content: text to write
+ * @text_content: The text content to append
+ * 					to the file
  *
  * Return: 1 on success 0 on failure
  */
